12d9bc5f09f085e9675c912007e2d9683b6aec64
Victoria0292 <tlynn0292@gmail.com>
2022-11-02 14:19:36 -0400

    initial commit BankingApp.java ScannerTest.java

---
+++ BankingApp.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BankingApp {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
+
+class BankAccount {
+	int balance;
+	int previousTransaction;
+	String customerName;
+	String customerID;
+	
+	void deposit(int amount) {
+		
+	}
+}
