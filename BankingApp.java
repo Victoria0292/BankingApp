b5e07c5f56e0215984ca00e3a3e9b28a3496cc8e
Victoria0292 <tlynn0292@gmail.com>
2022-11-02 14:58:23 -0400

    comment added to Math abs BankingApp.java

--- BankingApp.java
+++ BankingApp.java
@@ -1,50 +1,48 @@
 import java.util.Scanner;
 
 public class BankingApp {
-	
+
 	public static void main(String[] args) {
-		
-		
+
 	}
 }
-
 
 class BankAccount {
 	int balance;
 	int previousTransaction;
 	String customerName;
 	String customerID;
-	
-	//If amount is not equal to 0 we will add the balance and amount deposited	
+
+	// If amount is not equal to 0 we will add the balance and amount deposited
 	void deposit(int amount) {
 		if (amount != 0) {
 			balance += amount;
 			previousTransaction = amount;
 		}
 	}
-	
-	//If amount is not equal to 0 we will subtract the balance and amount withdrawn
+
+	// If amount is not equal to 0 we will subtract the balance and amount withdrawn
 	void withdraw(int amount) {
 		if (amount != 0) {
 			balance -= amount;
 			previousTransaction = -amount;
 		}
 	}
-	
+
+	//Prints one of three things depending on previousTransaction
 	void getPreviousTransaction() {
-		if(previousTransaction > 0) {
+		if (previousTransaction > 0) {
 			System.out.println("Deposited: " + previousTransaction);
-		} else if(previousTransaction < 0) {
+		} else if (previousTransaction < 0) {
+			//Math.abs returns the absolute value of an int
 			System.out.println("Withdrawn: " + Math.abs(previousTransaction));
 		} else {
 			System.out.println("No transaction occured");
 		}
 	}
-	
+
 	void showMenu() {
-		
+
 	}
-	
+
 }
-
-
