aee504b9b704d07f6162d8c9e207f918439c949c
Victoria0292 <tlynn0292@gmail.com>
2022-11-02 14:52:43 -0400

    add deposit, withdraw and previousTransaction BankingApp.java

--- BankingApp.java
+++ BankingApp.java
@@ -8,13 +8,43 @@ public class BankingApp {
 	}
 }
 
+
 class BankAccount {
 	int balance;
 	int previousTransaction;
 	String customerName;
 	String customerID;
 	
+	//If amount is not equal to 0 we will add the balance and amount deposited	
 	void deposit(int amount) {
+		if (amount != 0) {
+			balance += amount;
+			previousTransaction = amount;
+		}
+	}
+	
+	//If amount is not equal to 0 we will subtract the balance and amount withdrawn
+	void withdraw(int amount) {
+		if (amount != 0) {
+			balance -= amount;
+			previousTransaction = -amount;
+		}
+	}
+	
+	void getPreviousTransaction() {
+		if(previousTransaction > 0) {
+			System.out.println("Deposited: " + previousTransaction);
+		} else if(previousTransaction < 0) {
+			System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+		} else {
+			System.out.println("No transaction occured");
+		}
+	}
+	
+	void showMenu() {
 		
 	}
+	
 }
+
+
